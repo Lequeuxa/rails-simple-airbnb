8a88a4533ce665026b095c3205a95f849e50bd3f
Antoine Lequeux <lequeuxa@gmail.com>
2019-10-30 17:04:58 +0100

    first part

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,24 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+end
